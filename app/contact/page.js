afde22916122ad43541df0cf9dd8364c46a5b7fe
devsabiha <farjanasabiha786@gmail.com>
2025-08-10 00:44:31 +0600

    added header

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,7 +4,8 @@ import Header from '../Components/Header';
 const page = () => {
     return (
         <div>
-            <Header header={eefggf}/>
+            <Header header={eefggf} />
+            <h1>This is headser</h1>
         </div>
     );
 };
