bad9534c4daf89f2b168d6e3bac4ead7e45eb4fc
Minhaz Miah <minhaz.jsb.dhaka420@gmail.com>
2025-08-10 01:05:52 +0600

    testing

--- app/about/page.js
+++ app/about/page.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from '../Components/Header';
+import React from "react";
+import Header from "../Components/Header";
 
 const page = () => {
-    return (
-        <div>
-            <h1 className='text-primary'>This is about</h1>
+  return (
+    <div>
+      <h1 className="text-primary">This is Minhaz</h1>
 
-            <Header header='About header'/>
-        </div>
-    );
+      <Header header="About header" />
+    </div>
+  );
 };
 
 export default page;
