f10728e5900daeaad7d899cb7576e2c5e5c0319b
unknown <minhaz.jsb.dhaka420@gmail.com>
2025-08-10 23:40:56 +0600

    Resolved merge conflicts

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import { ReactLenis } from "lenis/react";
 import HomeHeader from "./Components/Home/HomeHeader";
+import Navbar from "./Components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <ReactLenis root>
+      <Navbar />
       <main>
         <HomeHeader />
       </main>
