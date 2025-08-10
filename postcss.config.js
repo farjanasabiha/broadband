a84ec0109299e73000db3e6aa44ea83f72e0cce5
devsabiha <farjanasabiha786@gmail.com>
2025-08-10 17:51:22 +0600

    Added

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,22 @@
 // }
 
 
+// module.exports = {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//     autoprefixer: {},
+//   },
+// };
+
+// postcss.config.js
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
