ccd7dc75049244a64966a83b05b4d07cd5b51f64
devsabiha <farjanasabiha786@gmail.com>
2025-08-19 21:16:47 +0600

    Fixed Issues

--- app/page.js
+++ app/page.js
@@ -2,15 +2,15 @@
 import { ReactLenis } from "lenis/react";
 import HomeHeader from "./Components/Home/HomeHeader";
 import HomeSlider from "./Components/Home/Slider/HomeSlider";
-import { Network } from "lucide-react";
+import Network  from "./Components/Home/Network";
 import Services from "./Components/Home/Services";
 import VideoAndServicesOne from "./Components/Home/VideoAndServicesOne";
 import VideoAndServicesTwo from "./Components/Home/VideoAndServicesTwo";
 import SpeedLanding from "./Components/Home/SpeedLanding";
 import HappyClientsSection from "./Components/Home/HappyClientsSection";
 import PricingCard from "./Components/Home/PricingCard";
-// import TopBar from "./Components/Common/Navbar/TopBar";
-// import Navbar from "./Components/Common/Navbar/Navbar";
+import TopBar from "./Components/Common/Navbar/TopBar";
+import Navbar from "./Components/Common/Navbar/Navbar";
 import Footer from "./Components/Common/Footer/Footer";
 
 import CTA from "./Components/Common/CTA";
@@ -21,8 +21,8 @@ export default function Home() {
   return (
     <ReactLenis root data-theme="none">
       <main className="space-y-24 font-sf">
-        {/* <TopBar />
-        <Navbar /> */}
+        <TopBar />
+        <Navbar />
         <HomeHeader />
         <HomeSlider />
         <SpeedTest />
