80224273eb0385c93a13e789230e61340beea078
devsabiha <farjanasabiha786@gmail.com>
2025-10-01 20:34:17 +0600

    Fix All

--- app/Components/Offers/Warning.jsx
+++ app/Components/Offers/Warning.jsx
@@ -4,15 +4,17 @@ import React from "react";
 const Warning = () => {
   return (
     <div className="container max-w-6xl mx-auto px-5">
-      <div className="card card-side bg-base-100 shadow-sm">
-        <figure className=" h-[0px] w-[0px] md:h-[170px] hidden md:w-[200px] bg-[#fecf00] px-5">
+      <div className="grid grid-cols-12 gap-5 card card-side bg-base-100 shadow-sm">
+<div className='col-span-12 md:col-span-3'>
+          <figure className=" h-full w-full md:h-[170px] hidden md:w-[200px] bg-[#fecf00] px-5">
           <img
-            className="h-[85px]  w-[92px]"
+            className="h-full  w-[200px]"
             src="/UniqueSizzlingFinwhale-max-1mb.gif"
             alt="Movie"
           />
         </figure>
-        <div className="card-body">
+</div>
+        <div className="col-span-12 md:col-span-9 card-body">
           <h2 className="card-title mb-4">
             The type of the offer and the validity can be changed anytime based
             on the situation.
