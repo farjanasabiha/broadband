02c166a476265065f86dce7f3907fb3ca1409c4f
devsabiha <farjanasabiha786@gmail.com>
2025-10-06 22:44:40 +0600

    Fix All

--- app/Components/BestOfferSylhet/Header.jsx
+++ app/Components/BestOfferSylhet/Header.jsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="relative w-full h-[50vh] sm:h-[60vh] md:h-[80vh] lg:h-screen overflow-hidden">
+    <div className="relative w-full h-full lg:h-screen">
       <Image
         src="/best-offer.jpg"
         alt="best-offer"
-        fill
-        priority
         quality={100}
-        sizes="100vw"
-        className="object-cover object-center"
+        unoptimized
+        height={500}
+        width={500}
+        className="w-full object-cover object-center"
       />
     </div>
   );
